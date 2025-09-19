791534990a1126cb15067964ebe0dfd07874254e
shinya.fujii <akapikumin08@gmail.com>
2025-09-19 23:31:30 +0900

    記事一覧の取得

--- environments/db/1_table.sql
+++ environments/db/1_table.sql
@@ -1,13 +1,13 @@
-USE manage_docs;
+use manage_docs;
 
-CREATE TABLE organization(
+create table organization(
     id char(36) primary key
 );
 
-create table organization_member(
+create table join_organization(
     organization_id char(36) references organization(id) on delete cascade,
     user_id char(36) references user(id) on delete cascade,
-    registered_at timestamp default now(),
+    registered_at timestamp not null,
     primary key (organization_id, user_id)
 );
 
@@ -18,10 +18,19 @@ create table user(
 
 create table user_icon(
     user_id char(36) primary key references user(id) on delete cascade,
-    icon_url text not null
+    icon_path text not null
 );
 
 create table article(
-    id char(36) primary key
+    id char(36) primary key,
+    title text not null,
+    content text not null
+);
+
+create table post_article(
+    article_id char(36) references article(id) on delete cascade,
+    user_id char(36) references user(id) on delete cascade,
+    registered_at timestamp not null,
+    primary key (article_id, user_id)
 );
 
