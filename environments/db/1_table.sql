d6ed0d06b52674608e01cb348c94238b302b1651
shinya.fujii <akapikumin08@gmail.com>
2025-09-20 18:13:49 +0900

    記事の更新

--- environments/db/1_table.sql
+++ environments/db/1_table.sql
@@ -4,21 +4,32 @@ create table organization(
     organization_id char(36) primary key
 );
 
-create table join_organization(
-    organization_id char(36) references organization(id) on delete cascade,
-    user_id char(36) references user(id) on delete cascade,
-    registered_at timestamp not null,
-    primary key (organization_id, user_id)
-);
-
 create table user(
     user_id char(36) primary key,
     name text not null
 );
 
+create table organization_member(
+    organization_id char(36) not null,
+    user_id char(36) not null,
+    primary key (organization_id, user_id),
+    foreign key (user_id) references user(user_id) on delete cascade,
+    foreign key (organization_id) references organization(organization_id) on delete cascade
+);
+
+create table join_organization(
+    organization_id char(36) not null,
+    user_id char(36) not null,
+    registered_at timestamp not null,
+    primary key (organization_id, user_id),
+    foreign key (user_id) references user(user_id) on delete cascade,
+    foreign key (organization_id) references organization(organization_id) on delete cascade
+);
+
 create table user_icon(
-    user_id char(36) primary key references user(id) on delete cascade,
-    icon_path text not null
+    user_id char(36) primary key,
+    icon_path text not null,
+    foreign key (user_id) references user(user_id) on delete cascade
 );
 
 create table article(
@@ -27,9 +38,24 @@ create table article(
     content text not null
 );
 
+create table article_owner(
+    article_id char(36) not null,
+    user_id char(36) not null,
+    organization_id char(36) not null,
+    primary key (article_id, user_id, organization_id),
+    foreign key (article_id) references article(article_id) on delete cascade,
+    foreign key (user_id) references user(user_id),
+    foreign key (organization_id) references organization(organization_id) on delete cascade
+);
+
+create table update_article(
+    article_id char(36) primary key,
+    updated_at timestamp not null,
+    foreign key (article_id) references article(article_id) on delete cascade
+);
+
 create table post_article(
-    article_id char(36) references article(id) on delete cascade,
-    owner_id char(36) references user(id) on delete cascade,
+    article_id char(36) primary key,
     registered_at timestamp not null,
-    primary key (article_id, owner_id)
+    foreign key (article_id) references article(article_id) on delete cascade
 );
